5a47a75fc6296f79338b7a54dd4086bfbb894310
Chris Claude <christ.tchambila@gmail.com>
2025-03-24 15:47:24 +0100

    Add redux toolkit

---
+++ README.md
@@ -0,0 +1,20 @@
+# BookMe
+This is a web application to allow customers to book appointments with a hairdresser.
+
+## Requirements
+
+| Requirement | Status |
+|------------|--------|
+| User Authentication | 🟡 In Progress |
+| UI (desktop and mobile) | 🟡 In Progress |
+| Appointment Booking | ❌ Not Started |
+| Calendar Integration | ❌ Not Started |
+| Email Notifications | ❌ Not Started |
+| Admin Dashboard | ❌ Not Started |
+| Service Management | ❌ Not Started |
+Language support (ENG and PL)| ❌ Not Started |
+
+Legend:
+- ✅ Completed
+- 🟡 In Progress
+- ❌ Not Started
---
+++ bookme-ui/src/app/utils/content.utils.ts
@@ -0,0 +1,28 @@
+export const CONTENT = {
+  en: {
+    home: {
+      title: 'BookMe',
+      description: 'Book your next haircut today!',
+      services: 'services',
+      gallery: 'gallery',
+      contact: 'contact',
+      bannerHeader: 'The best haircuts in town',
+      bannerDescription:
+        'We offer a wide range of haircuts and styles to suit your needs. We are dedicated to make your hair and beard look its best.',
+      bookingActionButton: 'Book now',
+    },
+  },
+  pl: {
+    home: {
+      title: 'BookMe',
+      description: 'Zarezerwuj swój następny fryzjer!',
+      services: 'usługi',
+      gallery: 'galeria',
+      contact: 'kontakt',
+      bannerHeader: 'Najlepsze fryzury w mieście',
+      bannerDescription:
+        'Oferujemy szeroki wybór fryzur i stylów, aby spełnić Twoje potrzeby. Dbamy o to, aby Twój włos i broda wyglądały na najlepszy możliwy sposób.',
+      bookingActionButton: 'Zarezerwuj teraz',
+    },
+  },
+};

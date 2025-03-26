6fcf1c8390c8596689235d827cd837ee7a7541f7
Chris Claude <christ.tchambila@gmail.com>
2025-03-26 14:02:14 +0100

    Improve banner

--- bookme-ui/src/app/(default-layout)/page.tsx
+++ bookme-ui/src/app/(default-layout)/page.tsx
@@ -2,13 +2,14 @@
 import useLanguageState from '@/_hooks/useLanguageState';
 import { CONTENT } from '@/_lib/utils/content.utils';
 import { Button } from '@heroui/react';
+import Image from 'next/image';
 
 export default function Home() {
   const { language } = useLanguageState();
 
   return (
     <>
-      <main className="py-28 px-48 banner-bg flex flex-col gap-y-16 h-[50rem]">
+      <main className="relative py-28 px-48 banner-bg flex flex-col gap-y-16 h-[50rem]">
         <div className="flex flex-col gap-y-8 lg:w-[44rem] w-full">
           <h1 className="text-6xl font-bold uppercase">
             {CONTENT[language].home.bannerHeader}
@@ -24,6 +25,15 @@ export default function Home() {
             className="px-12 py-8 text-lg uppercase">
             {CONTENT[language].home.bookingActionButton}
           </Button>
+        </div>
+
+        <div className="absolute bottom-0  right-40">
+          <Image
+            src="/img/banner_image.png"
+            width={500}
+            height={500}
+            alt="Picture of a barber"
+          />
         </div>
       </main>
       <button className="action-button">

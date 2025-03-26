32b29deb4f0543bc2cd64c2645bb84d63c1eecb1
Chris Claude <christ.tchambila@gmail.com>
2025-03-26 13:30:42 +0100

    Add book now action button animation

--- bookme-ui/src/app/(default-layout)/page.tsx
+++ bookme-ui/src/app/(default-layout)/page.tsx
@@ -7,7 +7,7 @@ export default function Home() {
   const { language } = useLanguageState();
 
   return (
-    <div>
+    <>
       <main className="py-28 px-48 banner-bg flex flex-col gap-y-16 h-[50rem]">
         <div className="flex flex-col gap-y-8 lg:w-[44rem] w-full">
           <h1 className="text-6xl font-bold uppercase">
@@ -26,6 +26,9 @@ export default function Home() {
           </Button>
         </div>
       </main>
-    </div>
+      <button className="action-button">
+        {CONTENT[language].home.bookingActionButton}
+      </button>
+    </>
   );
 }

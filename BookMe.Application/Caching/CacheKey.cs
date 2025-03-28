142d8ad6670e0998eb2b65634f78560ef69a1609
Chris Claude <christ.tchambila@gmail.com>
2025-03-28 17:16:03 +0100

    Add repository and cache key service interfaces

---
+++ BookMe.Application/Caching/CacheKey.cs
@@ -0,0 +1,55 @@
+using System;
+
+namespace BookMe.Application.Caching;
+
+public class CacheKey
+{
+  #region Ctor
+
+  /// <summary>
+  /// Initialize a new instance with key and prefixes
+  /// </summary>
+  /// <param name="key">Key</param>
+  public CacheKey(string key)
+  {
+    Key = key;
+  }
+
+  #endregion
+
+  #region Methods
+
+  /// <summary>
+  /// Create a new instance from the current one and fill it with passed parameters
+  /// </summary>
+  /// <param name="createCacheKeyParameters">Function to create parameters</param>
+  /// <param name="keyObjects">Objects to create parameters</param>
+  /// <returns>Cache key</returns>
+  public virtual CacheKey Create(Func<object, object> createCacheKeyParameters, params object[] keyObjects)
+  {
+    var cacheKey = new CacheKey(Key);
+
+    if (!keyObjects.Any())
+      return cacheKey;
+
+    cacheKey.Key = string.Format(cacheKey.Key, keyObjects.Select(createCacheKeyParameters).ToArray());
+
+    return cacheKey;
+  }
+
+  #endregion
+
+  #region Properties
+
+  /// <summary>
+  /// Gets or sets a cache key
+  /// </summary>
+  public string Key { get; protected set; }
+
+  /// <summary>
+  /// Gets or sets a cache time in minutes
+  /// </summary>
+  public int CacheTime { get; set; } = 15; // TODO: Make this configurable
+
+  #endregion
+}
--- BookMe.Application/Interfaces/IRepository.cs
+++ BookMe.Application/Interfaces/IRepository.cs
@@ -1,8 +1,76 @@
 using System;
+using BookMe.Application.Caching;
+using BookMe.Application.Entities;
 
 namespace BookMe.Application.Interfaces;
 
-public interface IRepository
+/// <summary>
+/// Represents an entity repository
+/// </summary>
+/// <typeparam name="TEntity">Entity type</typeparam>
+public interface IRepository<TEntity> where TEntity : BaseEntity
 {
+    #region Methods
 
+    Task<TEntity> GetByIdAsync(int? id, Func<ICacheKeyService, CacheKey> getCacheKey = null, bool includeDeleted = true, bool useShortTermCache = false);
+
+
+    TEntity GetById(int? id, Func<ICacheKeyService, CacheKey> getCacheKey = null, bool includeDeleted = true);
+
+
+    Task<IList<TEntity>> GetByIdsAsync(IList<int> ids, Func<ICacheKeyService, CacheKey> getCacheKey = null, bool includeDeleted = true);
+
+
+    Task<IList<TEntity>> GetAllAsync(Func<IQueryable<TEntity>, IQueryable<TEntity>> func = null,
+        Func<ICacheKeyService, CacheKey> getCacheKey = null, bool includeDeleted = true);
+
+
+    Task<IList<TEntity>> GetAllAsync(Func<IQueryable<TEntity>, Task<IQueryable<TEntity>>> func = null,
+        Func<ICacheKeyService, CacheKey> getCacheKey = null, bool includeDeleted = true);
+
+
+    Task<IList<TEntity>> GetAllAsync(Func<IQueryable<TEntity>, Task<IQueryable<TEntity>>> func,
+        Func<ICacheKeyService, Task<CacheKey>> getCacheKey, bool includeDeleted = true);
+
+
+    Task<IPagedList<TEntity>> GetAllPagedAsync(Func<IQueryable<TEntity>, IQueryable<TEntity>> func = null,
+        int pageIndex = 0, int pageSize = int.MaxValue, bool getOnlyTotalCount = false, bool includeDeleted = true);
+
+
+    Task<IPagedList<TEntity>> GetAllPagedAsync(Func<IQueryable<TEntity>, Task<IQueryable<TEntity>>> func = null,
+        int pageIndex = 0, int pageSize = int.MaxValue, bool getOnlyTotalCount = false, bool includeDeleted = true);
+
+
+    Task InsertAsync(TEntity entity, bool publishEvent = true);
+
+
+
+    Task InsertAsync(IList<TEntity> entities, bool publishEvent = true);
+
+
+
+    Task UpdateAsync(TEntity entity, bool publishEvent = true);
+
+
+
+
+    Task UpdateAsync(IList<TEntity> entities, bool publishEvent = true);
+
+
+
+    Task DeleteAsync(TEntity entity, bool publishEvent = true);
+
+    Task DeleteAsync(IList<TEntity> entities, bool publishEvent = true);
+
+
+    #endregion
+
+    #region Properties
+
+    /// <summary>
+    /// Gets a table
+    /// </summary>
+    IQueryable<TEntity> Table { get; }
+
+    #endregion
 }

6db41553796d1fa24f623e4f814c6c624cf3b740
Chris Claude <christ.tchambila@gmail.com>
2025-03-30 20:04:02 +0200

    Update page.tsx

--- bookme-ui/src/app/(default-layout)/[lang]/page.tsx
+++ bookme-ui/src/app/(default-layout)/[lang]/page.tsx
@@ -1,14 +1,12 @@
 'use client';
 import HomePageContent from '@/_components/HomePageContent';
-import Loader from '@/_components/Loader';
 import useLanguageState from '@/_hooks/useLanguageState';
 import { useRouter } from 'next/navigation';
-import { useEffect, useState } from 'react';
+import { useEffect } from 'react';
 
 const HomeLang = ({ params }: { params: { lang: string } }) => {
   const router = useRouter();
   const { language, switchLanguage } = useLanguageState();
-  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
     if (params.lang === 'en' || params.lang === 'pl') {
@@ -18,13 +16,6 @@ const HomeLang = ({ params }: { params: { lang: string } }) => {
     router.push('/');
   }, [params.lang, router, switchLanguage]);
 
-  useEffect(() => {
-    setIsLoading(false);
-  }, [language]);
-
-  if (isLoading) {
-    return <Loader />;
-  }
 
   return <HomePageContent language={language} />;
 };

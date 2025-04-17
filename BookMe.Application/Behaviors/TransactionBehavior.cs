b5183b534eea5aa9c31d05c77b8ebcfe02b31550
Chris Claude <christ.tchambila@gmail.com>
2025-04-17 19:52:31 +0200

    Create TransactionBehavior.cs

---
+++ BookMe.Application/Behaviors/TransactionBehavior.cs
@@ -0,0 +1,45 @@
+using System;
+using BookMe.Application.Interfaces;
+using MediatR;
+using Serilog;
+
+namespace BookMe.Application.Behaviors;
+
+public class TransactionBehavior<TRequest, TResponse> : IPipelineBehavior<TRequest, TResponse> where TRequest : IRequest<TResponse>
+{
+    private readonly ITransactionManager _transactionManager;
+
+    public TransactionBehavior(ITransactionManager transactionManager)
+    {
+        _transactionManager = transactionManager;
+    }
+
+    public async Task<TResponse> Handle(TRequest request, RequestHandlerDelegate<TResponse> next, CancellationToken cancellationToken)
+    {
+        var typeName = typeof(TRequest).Name;
+        Guid? transactionId = null;
+
+        try
+        {
+            if (!_transactionManager.HasActiveTransaction)
+            {
+                transactionId = await _transactionManager.BeginTransactionAsync
+                (cancellationToken);
+                Log.Information("Transaction started with ID: {TransactionId} for {CommandType}", transactionId, typeName);
+            }
+
+            var response = await next(cancellationToken);
+
+            Log.Information("Committing transaction for {CommandType} with ID: {TransactionId}", typeName, transactionId);
+            await _transactionManager.CommitTransactionAsync(cancellationToken);
+
+            return response;
+        }
+        catch (Exception ex)
+        {
+            await _transactionManager.RollbackTransactionAsync(cancellationToken);
+            Log.Error(ex, "Transaction rolled back for {CommandType} with ID: {TransactionId}", typeName, transactionId);
+            throw;
+        }
+    }
+}

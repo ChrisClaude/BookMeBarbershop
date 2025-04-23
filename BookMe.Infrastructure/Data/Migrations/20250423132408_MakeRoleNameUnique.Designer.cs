58c74774ab6f08a05dba9e471ab0c47058e6c360
Chris Claude <christ.tchambila@gmail.com>
2025-04-23 15:25:45 +0200

    Make role name unique migrations

--- BookMe.Infrastructure/Data/Migrations/20250423132408_MakeRoleNameUnique.Designer.cs
+++ BookMe.Infrastructure/Data/Migrations/20250423132408_MakeRoleNameUnique.Designer.cs
@@ -4,6 +4,7 @@ using BookMe.Infrastructure.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace BookMe.Infrastructure.Data.Migrations
 {
     [DbContext(typeof(BookMeContext))]
-    partial class BookMeContextModelSnapshot : ModelSnapshot
+    [Migration("20250423132408_MakeRoleNameUnique")]
+    partial class MakeRoleNameUnique
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ BookMe.Infrastructure/Data/Migrations/20250423132408_MakeRoleNameUnique.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace BookMe.Infrastructure.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class MakeRoleNameUnique : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Roles",
+                type: "nvarchar(450)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.InsertData(
+                table: "Roles",
+                columns: new[] { "Id", "Name" },
+                values: new object[,]
+                {
+                    { new Guid("6b29fc40-ca47-1067-b31d-00dd010662fc"), "Customer" },
+                    { new Guid("6b29fc40-ca47-1067-b31d-00dd010662fd"), "Admin" }
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Roles_Name",
+                table: "Roles",
+                column: "Name",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Roles_Name",
+                table: "Roles");
+
+            migrationBuilder.DeleteData(
+                table: "Roles",
+                keyColumn: "Id",
+                keyValue: new Guid("6b29fc40-ca47-1067-b31d-00dd010662fc"));
+
+            migrationBuilder.DeleteData(
+                table: "Roles",
+                keyColumn: "Id",
+                keyValue: new Guid("6b29fc40-ca47-1067-b31d-00dd010662fd"));
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Roles",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(450)");
+        }
+    }
+}

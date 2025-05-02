5f212a7ad9a6b07bf3afc10c6eef6095084b91d1
Chris Claude <christ.tchambila@gmail.com>
2025-05-02 18:02:45 +0200

    Create BookingController.cs

---
+++ BookMeAPI/Controllers/BookingController.cs
@@ -0,0 +1,51 @@
+using BookMe.Application.Commands.Bookings;
+using BookMe.Application.Common;
+using BookMe.Application.Common.Bookings.Dtos;
+using BookMe.Application.Common.Dtos;
+using BookMe.Application.Common.Dtos.Bookings;
+using BookMe.Application.Entities;
+using BookMe.Application.Interfaces.Queries;
+using BookMeAPI.Extensions;
+using MediatR;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BookMeAPI.Controllers;
+[Route("api/[controller]")]
+[ApiController]
+public class BookingController(IMediator mediator, ITimeSlotQueries timeSlotQueries) : BaseController
+{
+    [HttpGet("/timeslots")]
+    [AllowAnonymous]
+    [ProducesResponseType<Result<IEnumerable<TimeSlotDto>>>(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    public async Task<IActionResult> GetAvailableTimeSlotsAsync(GetAvailableTimeSlotsDto request)
+    {
+        var result = await timeSlotQueries.GetAvailableTimeSlotsAsync(request.Start, request.End);
+
+        return result.ToActionResult();
+    }
+
+    [HttpPost]
+    [ProducesResponseType<BookingDto>(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+    public async Task<IActionResult> BookTimeSlotsAsync(BookTimeSlotsDto request)
+    {
+        var result = await mediator.Send(new CreateBookingCommand(request.TimeSlotId));
+
+        return result.ToActionResult();
+    }
+
+    [HttpPost("timeslots")]
+    [ProducesResponseType<TimeSlot>(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+    public async Task<IActionResult> CreateTimeSlotsAsync(CreateTimeSlotsDto request)
+    {
+        var result = await mediator.Send(new CreateTimeSlotCommand(request.StartDateTime, request.EndDateTime));
+
+        return result.ToActionResult();
+    }
+}
+

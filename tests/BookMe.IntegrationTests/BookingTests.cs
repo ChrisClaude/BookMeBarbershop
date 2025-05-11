f2060337f50302beba43e0692cc1ced0ad8a0544
Chris Claude <christ.tchambila@gmail.com>
2025-05-11 22:40:15 +0200

    Add CreateTimeSlotsWithNonAdminUser_ShouldNotSucceedAsync integration test

--- tests/BookMe.IntegrationTests/BookingTests.cs
+++ tests/BookMe.IntegrationTests/BookingTests.cs
@@ -7,6 +7,7 @@ using BookMe.Application.Mappings;
 using BookMe.IntegrationTests.TestData;
 using BookMeAPI.Controllers;
 using FluentAssertions;
+using FluentValidation;
 using MediatR;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
@@ -57,5 +58,36 @@ public class BookingTests : BaseIntegrationTest
             .ToListAsync();
 
         timeSlots.Should().HaveCount(1);
+    }
+
+    [Fact]
+    public async Task CreateTimeSlotsWithNonAdminUser_ShouldNotSucceedAsync()
+    {
+        // Arrange
+        var customerUser = await _bookMeContext.Users
+            .Include(x => x.UserRoles)
+            .ThenInclude(x => x.Role)
+            .FirstAsync(x => x.UserRoles.Any(y => y.RoleId == DefaultRoles.CustomerId));
+
+        _mockHttpContext.SetUser(customerUser.MapToDto());
+        var mediator = _scope.ServiceProvider.GetRequiredService<IMediator>();
+        var timeSlotQueries = _scope.ServiceProvider.GetRequiredService<ITimeSlotQueries>();
+        var bookingController = new BookingController(mediator, timeSlotQueries);
+
+        var createTimeSlotsCommand = new CreateTimeSlotsDto
+        {
+            StartDateTime = DateTime.UtcNow.AddDays(10).AddHours(1),
+            EndDateTime = DateTime.UtcNow.AddDays(10).AddHours(2),
+        };
+
+        // Act & Assert should throw exception because user is not admin
+        var exception = await Assert.ThrowsAsync<ValidationException>(() => bookingController.CreateTimeSlotsAsync(createTimeSlotsCommand));
+
+        exception.Message.Should().Be($"Validation failed: \n -- UserDTo: User {customerUser.Id} is not an admin");
+
+        var timeSlots = await _bookMeContext.TimeSlots
+            .ToListAsync();
+
+        timeSlots.Should().HaveCount(0);
     }
 }

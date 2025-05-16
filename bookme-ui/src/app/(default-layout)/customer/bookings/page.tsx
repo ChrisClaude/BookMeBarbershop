ce94e96c054cf0720452e3f497f361a6f616debd
Chris Claude <christ.tchambila@gmail.com>
2025-05-16 17:01:13 +0200

    Add booking form

---
+++ bookme-ui/src/app/(default-layout)/customer/bookings/page.tsx
@@ -0,0 +1,21 @@
+"use client";
+import React from "react";
+import { withAuth } from "@/_components/auth/AuthGuard";
+import Header from "@/_components/Header";
+import BookingForm from "@/_components/BookingForm";
+
+const BookingsPage = () => {
+
+  return (
+    <>
+      <Header />
+      <section className="py-12 md:py-20 lg:py-24 px-4 md:px-8 lg:px-48">
+        <BookingForm />
+      </section>
+    </>
+  );
+};
+
+export default withAuth(BookingsPage, "Customer", {
+  fallbackPath: "/unauthorized",
+});

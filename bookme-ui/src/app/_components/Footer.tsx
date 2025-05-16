61d95df15e06a772af6ea55a2523fbd40f514396
Chris Claude <christ.tchambila@gmail.com>
2025-05-16 17:00:47 +0200

    Add admin pages

--- bookme-ui/src/app/_components/Footer.tsx
+++ bookme-ui/src/app/_components/Footer.tsx
@@ -1,10 +1,11 @@
 'use client';
 import React from 'react';
 import { CONTENT } from '@/_lib/utils/content.utils';
+import { twMerge } from 'tailwind-merge';
 
-const Footer = () => {
+const Footer = ({className}: { className?: string }) => {
   return (
-    <footer className="bg-foreground text-background py-8 text-center text-sm">
+    <footer className={twMerge("bg-foreground text-background py-8 text-center text-sm", className)}>
       <p>
         &copy; {new Date().getFullYear()} {CONTENT.brandName}. All rights
         reserved.
---
+++ bookme-ui/src/app/_components/admin/Sidebar.tsx
@@ -0,0 +1,52 @@
+"use client";
+import React from "react";
+import Footer from "../Footer";
+import Link from "next/link";
+import Logo from "../Logo";
+import { usePathname } from "next/navigation";
+
+const Sidebar = () => {
+  const pathName = usePathname();
+  console.log("pathname", pathName);
+
+  const isActive = (href: string) => {
+    return pathName === href;
+  };
+
+  return (
+    <aside className="h-full w-48 bg-gray-300 flex flex-col">
+      <div className="bg-gray-300 p-4">
+        <Logo />
+      </div>
+      <nav className="flex flex-col">
+        <Link
+          href="/admin"
+          className={`block p-2 hover:bg-gray-200 ${
+            isActive("/admin") ? "bg-gray-200" : ""
+          }`}
+        >
+          Dashboard
+        </Link>
+        <Link
+          href="/admin/users"
+          className={`block p-2 hover:bg-gray-200 ${
+            isActive("/admin/users") ? "bg-gray-200" : ""
+          }`}
+        >
+          Users
+        </Link>
+        <Link
+          href="/admin/users"
+          className={`block p-2 hover:bg-gray-200 ${
+            isActive("/admin/bookings") ? "bg-gray-200" : ""
+          }`}
+        >
+          Bookings
+        </Link>
+      </nav>
+      <Footer className="py-4 mt-auto text-foreground bg-gray-300" />
+    </aside>
+  );
+};
+
+export default Sidebar;

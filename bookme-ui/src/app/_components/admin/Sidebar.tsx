9ef774c8e0d1fe5007edc11f285d40cb2382af6a
Chris Claude <christ.tchambila@gmail.com>
2025-05-19 20:02:34 +0200

    Improve admin sidebar

--- bookme-ui/src/app/_components/admin/Sidebar.tsx
+++ bookme-ui/src/app/_components/admin/Sidebar.tsx
@@ -4,6 +4,9 @@ import Footer from "../Footer";
 import Link from "next/link";
 import Logo from "../Logo";
 import { usePathname } from "next/navigation";
+import { Button } from "@heroui/react";
+import { CgProfile } from "react-icons/cg";
+import { useAuth } from "@/_hooks/useAuth";
 
 const Sidebar = () => {
   const pathName = usePathname();
@@ -12,12 +15,27 @@ const Sidebar = () => {
     return pathName === href;
   };
 
+  const { logout, userProfile } = useAuth();
+
   return (
     <aside className="h-full w-48 bg-gray-300 flex flex-col">
-      <div className="bg-gray-300 p-4">
+      <div className="p-4 flex flex-col gap-4">
         <Link href="/admin">
           <Logo />
         </Link>
+        <div className="flex flex-col gap-2 items-center">
+          <CgProfile size={60} className="text-primary" />
+          <div className="flex flex-col gap-1">
+            <p className="text-sm text-center">Admin</p>
+            <p className="text-xs text-gray-500 text-center">{userProfile?.email}</p>
+          </div>
+          <Button
+            onPress={() => logout()}
+            className="hover:bg-red-600 hover:text-white"
+          >
+            Log out
+          </Button>
+        </div>
       </div>
       <nav className="flex flex-col">
         <Link

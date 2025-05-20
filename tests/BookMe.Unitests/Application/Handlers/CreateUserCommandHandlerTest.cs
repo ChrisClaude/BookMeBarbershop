a84bcf54e59957ea65eed3c259bc09b539255d66
Chris Claude <christ.tchambila@gmail.com>
2025-05-20 11:19:47 +0200

    Fix unit test

--- tests/BookMe.Unitests/Application/Handlers/CreateUserCommandHandlerTest.cs
+++ tests/BookMe.Unitests/Application/Handlers/CreateUserCommandHandlerTest.cs
@@ -32,12 +32,30 @@ public class CreateUserCommandHandlerTest
             "1000000000"
         );
 
-        // Setup the repository to populate Role when InsertAsync is called
+        var userId = Guid.NewGuid();
+        var insertedUser = new User
+        {
+            Id = userId,
+            Name = "John",
+            Surname = "Doe",
+            Email = "john.doe@example.com",
+            PhoneNumber = "1000000000",
+            UserRoles = new List<UserRole>
+            {
+                new()
+                {
+                    RoleId = DefaultRoles.CustomerId,
+                    Role = new Role { Id = DefaultRoles.CustomerId, Name = RoleName.CUSTOMER }
+                }
+            }
+        };
+
         _repository
             .Setup(r => r.InsertAsync(It.IsAny<User>(), false))
             .Callback<User, bool>(
                 (user, _) =>
                 {
+                    user.Id = userId;
                     user.UserRoles[0].Role = new Role
                     {
                         Id = DefaultRoles.CustomerId,
@@ -45,6 +63,10 @@ public class CreateUserCommandHandlerTest
                     };
                 }
             );
+
+        _repository
+            .Setup(x => x.GetByIdAsync(userId, It.IsAny<string[]>(), null, true))
+            .ReturnsAsync(insertedUser);
 
         // Act
         var result = await _handler.Handle(createUserCommand, CancellationToken.None);
@@ -68,6 +90,24 @@ public class CreateUserCommandHandlerTest
                 ),
             Times.Once
         );
+        _repository.Verify(
+            x =>
+                x.GetByIdAsync(
+                    userId,
+                    It.Is<string[]>(
+                        x =>
+                            (
+                                x.Length == 2
+                                && x[0] == nameof(User.UserRoles)
+                                && x[1] == $"{nameof(User.UserRoles)}.{nameof(UserRole.Role)}"
+                            )
+                    ),
+                    null,
+                    true
+                ),
+            Times.Once
+        );
+
         result.IsSuccess.Should().BeTrue();
         result.Value.Should().BeOfType(typeof(UserDto));
         result.Value.Roles.Count().Should().Be(1);

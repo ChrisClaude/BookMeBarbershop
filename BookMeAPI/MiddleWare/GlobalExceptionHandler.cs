1c8c06cc5dde4e18120417effc793a05b22617ea
Chris Claude <christ.tchambila@gmail.com>
2025-05-29 18:04:18 +0200

    Handle validation exception and return properly formatted error

--- BookMeAPI/MiddleWare/GlobalExceptionHandler.cs
+++ BookMeAPI/MiddleWare/GlobalExceptionHandler.cs
@@ -1,4 +1,6 @@
 using System.Net;
+using BookMe.Application.Common;
+using FluentValidation;
 using Microsoft.AspNetCore.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 using Serilog;
@@ -7,8 +9,30 @@ namespace BookMeAPI.MiddleWare;
 
 public class GlobalExceptionHandler : IExceptionHandler
 {
-    public async ValueTask<bool> TryHandleAsync(HttpContext httpContext, Exception exception, CancellationToken cancellationToken)
+    public async ValueTask<bool> TryHandleAsync(
+        HttpContext httpContext,
+        Exception exception,
+        CancellationToken cancellationToken
+    )
     {
+        if (exception is ValidationException validationException)
+        {
+            Log.Warning(
+                "Validation exception occurred {@ValidationException}",
+                validationException
+            );
+
+            httpContext.Response.StatusCode = StatusCodes.Status400BadRequest;
+
+            var errors = validationException.Errors
+                .Select(x => new Error(x.Severity.ToString(), x.ErrorMessage))
+                .ToList();
+
+            await httpContext.Response.WriteAsJsonAsync(errors, cancellationToken);
+
+            return true;
+        }
+
         Log.Error(exception, "An unhandled exception occurred");
 
         var problemDetails = new ProblemDetails
@@ -20,8 +44,7 @@ public class GlobalExceptionHandler : IExceptionHandler
         httpContext.Response.StatusCode = problemDetails.Status.Value;
         httpContext.Response.ContentType = "application/json";
 
-        await httpContext.Response
-            .WriteAsJsonAsync(problemDetails, cancellationToken);
+        await httpContext.Response.WriteAsJsonAsync(problemDetails, cancellationToken);
 
         return true;
     }

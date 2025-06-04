5571285952112242ce54ef3b6d8283485939fa95
Chris Claude <christ.tchambila@gmail.com>
2025-06-04 23:15:28 +0200

    Fix header issue

--- bookme-ui/src/app/(default-layout)/customer/bookings/page.tsx
+++ bookme-ui/src/app/(default-layout)/customer/bookings/page.tsx
@@ -3,10 +3,12 @@ import React from "react";
 import { withAuth } from "@/_components/auth/AuthGuard";
 import BookingForm from "@/_components/booking-form/BookingForm";
 import { ROLES } from "@/_lib/enums/constant";
+import Header from "@/_components/Header";
 
 const BookingsPage = () => {
   return (
     <>
+      <Header />
       <section className="py-12 md:py-20 lg:py-24 px-4 md:px-8 lg:px-48">
         <BookingForm />
       </section>
--- bookme-ui/src/app/(default-layout)/layout.tsx
+++ bookme-ui/src/app/(default-layout)/layout.tsx
@@ -1,11 +1,9 @@
 import React from "react";
 import Footer from "@/_components/Footer";
-import Header from "@/_components/Header";
 
 const DefaultLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <div className="flex flex-col min-h-screen">
-      <Header />
       <main className="flex-grow">{children}</main>
       <Footer />
     </div>

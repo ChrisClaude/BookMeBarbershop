25177de169cd38b779dcc53dde8ad0ec8fbff791
Chris Claude <christ.tchambila@gmail.com>
2025-06-05 21:23:20 +0200

    add customer layout

--- bookme-ui/src/app/(default-layout)/customer/book-appointment/page.tsx
+++ bookme-ui/src/app/(default-layout)/customer/book-appointment/page.tsx
@@ -3,12 +3,10 @@ import React from "react";
 import { withAuth } from "@/_components/auth/AuthGuard";
 import BookingForm from "@/_components/booking-form/BookingForm";
 import { ROLES } from "@/_lib/enums/constant";
-import Header from "@/_components/header/Header";
 
 const BookAppointmentPage = () => {
   return (
     <>
-      <Header />
       <section className="py-12 md:py-20 lg:py-24 px-4 md:px-8 lg:px-48">
         <BookingForm />
       </section>

1686604414152ddb4f6e219b62c2d96e70f1416b
Chris Claude <christ.tchambila@gmail.com>
2025-06-06 15:28:44 +0200

    Update query to get bookings to sort descending by start date time for pending and confirmed bookings

--- BookMe.Application/Queries/BookingQueries.cs
+++ BookMe.Application/Queries/BookingQueries.cs
@@ -18,31 +18,18 @@ public class BookingQueries(IRepository<Booking> repository) : IBookingQueries
         int pageSize = 10
     )
     {
-        IPagedList<Booking> bookings;
-        if (bookingStatus == null)
-        {
-            bookings = await repository.GetAllPagedAsync(
-                query =>
-                    query
-                        .Where(x => x.TimeSlot.Start >= fromDateTime && x.User.Id == userId)
-                        .OrderBy(x => x.TimeSlot.Start),
-                includes: new[] { nameof(Booking.User), nameof(Booking.TimeSlot) },
-                pageIndex: pageIndex,
-                pageSize: pageSize
-            );
-
-            return Result.Success(bookings.MapToDto());
-        }
-
-        bookings = await repository.GetAllPagedAsync(
-            query =>
-                query
-                    .Where(x =>
-                        x.TimeSlot.Start >= fromDateTime
-                        && x.Status == bookingStatus
-                        && x.User.Id == userId
+        var bookings = await repository.GetAllPagedAsync(
+            queryable =>
+                queryable
+                    .Where(x => x.User.Id == userId)
+                    .Where(x => x.TimeSlot.Start >= fromDateTime)
+                    .Where(x => bookingStatus == null || x.Status == bookingStatus)
+                    .OrderBy(x =>
+                        x.Status == BookingStatus.Confirmed || x.Status == BookingStatus.Pending
+                            ? 0
+                            : 1
                     )
-                    .OrderBy(x => x.TimeSlot.Start),
+                    .ThenBy(x => x.TimeSlot.Start),
             includes: new[] { nameof(Booking.User), nameof(Booking.TimeSlot) },
             pageIndex: pageIndex,
             pageSize: pageSize

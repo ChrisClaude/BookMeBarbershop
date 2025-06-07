1a6075f2ac474d1ef967d0053810ee919cdc2c1d
Chris Claude <christ.tchambila@gmail.com>
2025-06-07 23:03:55 +0200

    Update TimeSlotQueriesTests.cs

--- tests/BookMe.IntegrationTests/TimeSlotQueriesTests.cs
+++ tests/BookMe.IntegrationTests/TimeSlotQueriesTests.cs
@@ -6,4 +6,6 @@ public class TimeSlotQueryTests : BaseIntegrationTest
         : base(factory) { }
 
     // TODO: Add a test whereby an admin view a list of timeslots and booked timeslots should show as not available while available timeslots should show as available
+
+    // TODO: Add tests where dates with available timeslots are returned
 }

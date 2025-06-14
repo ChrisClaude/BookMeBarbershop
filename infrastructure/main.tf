7ea210c4fe1df5a07dc9b1338c0e9e28071a689b
Chris Claude <christ.tchambila@gmail.com>
2025-06-14 09:51:33 +0200

    Add db connection string to keyvault

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -52,6 +52,27 @@ resource "azurerm_linux_web_app" "app_service" {
   }
 }
 
+resource "azurerm_key_vault" "kv" {
+  name                       = "kv-book-me-${var.environment}-${var.location}"
+  location                   = azurerm_resource_group.rg.location
+  resource_group_name        = azurerm_resource_group.rg.name
+  tenant_id                  = data.azurerm_client_config.current.tenant_id
+  sku_name                   = "standard"
+  purge_protection_enabled   = true
+  soft_delete_retention_days = 7
+  enable_rbac_authorization  = false
+}
+
+# Retrieve information about the currently authenticated Azure client/account used by Terraform
+# Required for Key Vault and other AAD-aware resources
+data "azurerm_client_config" "current" {}
+
+resource "azurerm_key_vault_secret" "sql_admin_password" {
+  name         = "SqlConnectionString"
+  value        = "Server=${azurerm_sql_server.sql_server.fully_qualified_domain_name};Database=${azurerm_sql_database.sql_database.name};User ID=${azurerm_sql_server.sql_server.administrator_login};Password=${local.sql_password};Encrypt=true;Connection Timeout=30;"
+  key_vault_id = azurerm_key_vault.kv.id
+}
+
 // create an azure sql database dtu
 resource "azurerm_mssql_server" "sql_server" {
   name                         = "sql-server-book-me-${local.suffix}-001"

a70c26603a9a2e5a649de9a7fa9664f3f6d3ce3f
Chris Claude <christ.tchambila@gmail.com>
2025-06-16 08:53:54 +0200

    apply migrations through a script

---
+++ scripts/apply_migrations.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# Remove any quotes from the environment variables
+CONNECTION_STRING=$(echo $CONNECTION_STRING | tr -d '"')
+
+dotnet tool install --global dotnet-ef
+dotnet ef database update --context BookMeContext --project BookMe.Infrastructure --startup-project BookMeAPI --connection "$CONNECTION_STRING"
